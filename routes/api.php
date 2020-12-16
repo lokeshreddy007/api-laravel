c364141600282184639910f5a683d5f025cfca35
LokeshReddy007 <LokeshReddy160@gmail.com>
2020-12-16 14:01:47 +0530

    add API versioning V1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PersonController;
+use App\Http\Controllers\Api\v1\PersonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
@@ -19,4 +19,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::apiResource('/person', PersonController::class);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/person', PersonController::class);
+});
